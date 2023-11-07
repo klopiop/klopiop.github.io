54b5b3699f207880c7b2cc2e23eaf1a5a232a594
klopiop <2062769036@qq.com>
2023-11-07 22:37:08 +0800

    Site updated: 2023-11-07 22:37:07

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/11/06/hello-world/"];function toRandomPost(){
+var posts=["2023/11/07/2023-11-07-No.1hulhub-fastjson任意命令执行/","2023/11/06/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
